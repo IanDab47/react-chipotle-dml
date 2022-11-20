e3bfe3e05b6ced8c21d5acc6ee453b4c0d99c686
IanDab47 <dabinett.ian@gmail.com>
2022-11-20 14:48:58 -0600

    displays all menu items in grid for hot and cold screens

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // React
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 // Pages
 import Hot from "./Screens/Hot";
--- src/Components/menu/Menu.js
+++ src/Components/menu/Menu.js
@@ -2,9 +2,17 @@
 import "./styles.css"
 
 export default function Menu({ type, items }) {
+  const itemList = items.map(item => {
+    return (
+      <div key={`food_${item.name}`} className="menu-item" style={{'--row': item.row, '--col': item.col, '--span': item.span}}>
+        <p>{item.name}</p>
+      </div>
+    )
+  })
+
   return (
     <section className={`menu-container ${type}`}>
-      Menu
+      {itemList}
     </section>
   )
 }
