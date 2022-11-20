6877682531251fba22a49fa6ca2041f67989d331
IanDab47 <dabinett.ian@gmail.com>
2022-11-20 11:29:16 -0600

    adds function navbar

--- src/Components/nav/Navbar.js
+++ src/Components/nav/Navbar.js
@@ -1,12 +1,29 @@
+// React
+import { useState } from "react"
+
 // Styles
 import "./styles.css"
 
-export default function Navbar({ setScreen }) {
+export default function Navbar({ screen, setScreen }) {
+  const [active, setActive] = useState()
+
+  // Handler
+  const handleClick = (e) => {
+    console.log(e.target)
+    // Toggle between screen
+    if(active) active.classList.remove('active')
+    e.target.classList.add('active')
+    setActive(e.target)
+
+    // Change screen
+    setScreen(e.target.value)
+  }
+
   return (
     <nav className="navbar">
-      <p onClick={e => setScreen('hot')}>Hot</p>
-      <p onClick={e => setScreen('cold')}>Cold</p>
-      <p onClick={e => setScreen('bag')}>Bag</p>
+      <p className='' onClick={handleClick} value='hot'>Hot</p>
+      <p className='' onClick={handleClick} value='cold'>Cold</p>
+      <p className='' onClick={handleClick} value='bag'>Bag</p>
     </nav>
   )
 }
